2bf5aabd9ff4456397663480c3069ebc48c5fb64
true_mogician <925269713@qq.com>
2021-03-24 23:51:42 +0800

    feat: splitAt for String

--- index.ts
+++ index.ts
@@ -32,6 +32,8 @@ declare global {
 	}
 	interface String {
 		remove(from: number, length?: number): string;
+		splitAt(index: number, charAtIndex?: "pred" | "succ" | "both" | "none"): string[];
+		splitAt(indices: number[], charAtIndex?: "pred" | "succ" | "both" | "none"): string[]
 	}
 	interface ObjectConstructor {
 		isEmpty(value: {}): boolean;
@@ -282,6 +284,38 @@ String.prototype.remove = function (this: string, from: number, length?: number)
 		return this.substr(0, from) + this.substr(from + length)
 	else
 		return this.substr(0, from);
+}
+String.prototype.splitAt = function (this: string, indices: number | number[], charAtIndex: "pred" | "succ" | "both" | "none" = "succ"): string[] {
+	if (typeof indices == "number")
+		indices = [indices];
+	indices.sort((a, b) => a - b);
+	let start = 0, end = 1;
+	for (let negative = indices[0] < 0; end < indices.length; negative = indices[end++] < 0) {
+		if (negative)
+			start = end;
+		if (indices[end] > this.length)
+			break;
+	}
+	indices = indices.slice(start, end);
+	if (!indices.length)
+		return [this];
+	const result = new Array<string>();
+	const pred = (charAtIndex == "pred" || charAtIndex == "both") ? 1 : 0;
+	const succ = (charAtIndex == "succ" || charAtIndex == "both") ? 1 : 0;
+	let sub = this.substr(0, indices[0] + pred);
+	if (sub != "")
+		result.push(sub);
+	for (let i = 1; i < indices.length; ++i) {
+		if (indices[i] == indices[i - 1])
+			continue;
+		start = indices[i - 1] + 1 - succ, end = indices[i] + pred;
+		if (start < end)
+			result.push(this.substring(start, end));
+	}
+	sub = this.substr(indices.last() + 1 - succ);
+	if (sub != "")
+		result.push(sub);
+	return result;
 }
 
 Object.isEmpty = function (value: {}): boolean {
