b8e223c5da4acd7ac35019df2f51302cd74557a1
true_mogician <925269713@qq.com>
2021-03-26 22:50:22 +0800

    test: object

---
+++ test/object.test.ts
@@ -0,0 +1,78 @@
+import "../index"
+let obj1 = {
+	key1: true,
+	key2: 1,
+	key3: "a"
+};
+let obj2 = {
+	key2: 2,
+	key3: "b",
+	key4: []
+};
+let obj3 = {
+	key1: false,
+	key2: 3,
+	key5: {}
+};
+test("isEmpty", () => {
+	expect(Object.isEmpty([])).toBeTruthy();
+	expect(Object.isEmpty({})).toBeTruthy();
+	expect(Object.isEmpty(["a"])).toBeFalsy();
+	expect(Object.isEmpty(123)).toBeTruthy();
+});
+test("isNullOrUndefined", () => {
+	let obj!: object;
+	expect(Object.isNullOrUndefined(obj)).toBeTruthy();
+	expect(Object.isNullOrUndefined({})).toBeFalsy();
+	expect(Object.isNullOrUndefined(null)).toBeTruthy();
+})
+test("innerAssign", () => {
+	const temp = {};
+	Object.assign(temp, obj1);
+	expect(Object.innerAssign(temp, obj2)).toEqual({
+		key1: true,
+		key2: 2,
+		key3: "b"
+	});
+	Object.assign(temp, obj1);
+	expect(Object.innerAssign(temp, obj2, obj3)).toEqual({
+		key1: false,
+		key2: 3,
+		key3: "b"
+	});
+});
+describe("copy", () => {
+	test("primitive", () => {
+		expect(Object.clone(true)).toBe(true);
+		expect(Object.clone("copy")).toBe("copy");
+		expect(Object.clone(123)).toBe(123);
+	});
+	test("object", () => expect(Object.clone(obj1)).toEqual(obj1));
+	test("nested", () => {
+		const obj = {
+			0: true,
+			1: {
+				2: "2",
+				3: () => 3,
+				4: [false, 1, "2", { key: 3 }]
+			}
+		}
+		const replica = Object.clone(obj);
+		replica[0] = false;
+		expect(obj[0]).toBe(true);
+		replica[1][3] = () => null;
+		expect(obj[1][3]()).toBe(3);
+		replica[1][4].splice(2);
+		expect(obj[1][4].length).toBe(4);
+	});
+	test("symbol", () => {
+		const symbol = Symbol(0);
+		const obj = {
+			0: 0,
+			key: "value",
+			[symbol]: true
+		};
+		const replica = Object.clone(obj);
+		expect(replica[symbol]).toBe(true);
+	})
+});
