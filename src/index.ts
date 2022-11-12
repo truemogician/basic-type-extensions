ac0fa385031999832ba1f2af35e3ed2e57580ab2
true_mogician <truemogician@gmail.com>
2022-11-12 20:25:40 +0800

    feat[root]: form root package by importing sub packages

---
+++ src/index.ts
@@ -0,0 +1,8 @@
+import "@basic-type-extensions/array";
+import "@basic-type-extensions/array-constructor";
+import "@basic-type-extensions/math";
+import "@basic-type-extensions/promise-constructor";
+import "@basic-type-extensions/string-constructor";
+
+export type { AsyncOptions } from "@basic-type-extensions/array";
+export { CleanOption, type RightAssignOptions } from "@basic-type-extensions/object-constructor";
