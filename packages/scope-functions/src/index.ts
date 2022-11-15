264a3d40ea32292d5d359245ae62de41beb3e871
true_mogician <truemogician@gmail.com>
2022-11-16 01:05:14 +0800

    feat[scope-functions]: enable and disable scope functions as needed

--- packages/scope-functions/src/index.ts
+++ packages/scope-functions/src/index.ts
@@ -1,5 +1,3 @@
-export { }
-
 declare global {
 	interface Object {
 		/**
@@ -38,29 +36,108 @@ declare global {
 	}
 }
 
-Object.prototype.let = function <T, R = any>(this: T, action: (it: T) => R): R {
+function lett<T, R = any>(this: T, action: (it: T) => R): R {
 	return action(this);
-};
+}
 
-Object.prototype.run = function <T, R = any>(this: T, action: (this: T) => R): R {
+function run<T, R = any>(this: T, action: (this: T) => R): R {
 	return action.call(this);
-};
+}
 
-Object.prototype.apply = function <T>(this: T, action: (this: T) => any): T {
+function apply<T>(this: T, action: (this: T) => any): T {
 	action.call(this);
 	return this;
-};
+}
 
-Object.prototype.also = function <T>(this: T, action: (it: T) => any): T {
+function also<T>(this: T, action: (it: T) => any): T {
 	action(this);
 	return this;
-};
+}
 
-(function <T extends object, K extends keyof T>(obj: T, ...keys: K[]): void {
+function configure<T extends object, K extends keyof T>(obj: T, ...keys: K[]): void {
 	for (const key of keys)
 		Object.defineProperty(obj, key, {
 			configurable: true,
 			enumerable: false,
 			writable: false
 		});
-})(Object.prototype, "let", "run", "apply", "also");
+}
+
+let enabled = false;
+
+function enable() {
+	if (enabled)
+		return;
+	Object.prototype.let = lett;
+	Object.prototype.run = run;
+	Object.prototype.also = also;
+	Object.prototype.apply = apply;
+	configure(Object.prototype, "let", "run", "also", "apply");
+	enabled = true;
+}
+
+function disable() {
+	if (!enabled)
+		return;
+	const proto = Object.prototype as Partial<Object>;
+	delete proto.let;
+	delete proto.run;
+	delete proto.also;
+	delete proto.apply;
+	enabled = false;
+}
+
+/**
+ * Enable scope functions globally.
+ */
+export function enableScopeFunctions(): void;
+/**
+ * Temporarily enable scope functions until the given action completes.
+ * @param action Action to perform with scope functions enabled.
+ */
+export function enableScopeFunctions(action: () => void): void;
+/**
+ * Temporarily enable scope functions until the given asynchrnous action completes.
+ * @param action Action to perform with scope functions enabled.
+ * @returns A promise that resolves when the action completes.
+ * @warning Due to event loops, the temporarily enabled scope functions can be available and accessed outside of the action.
+ */
+export function enableScopeFunctions(action: () => Promise<any>): Promise<void>;
+export function enableScopeFunctions(action?: () => void | Promise<any>): void | Promise<void> {
+	enable();
+	if (!action)
+		return;
+	const result = action();
+	if (result instanceof Promise)
+		return result.then(disable);
+	disable();
+}
+
+/**
+ * Disable scope functions globally.
+ */
+export function disableScopeFunctions(): void;
+/**
+ * Temporarily disable scope functions until the given action completes.
+ * @param action Action to perform with scope functions disabled.
+ */
+export function disableScopeFunctions(action: () => void): void;
+/**
+ * Temporarily disable scope functions until the given asynchrnous action completes.
+ * @param action Action to perform with scope functions disabled.
+ * @returns A promise that resolves when the action completes.
+ * @warning Due to event loops, the temporarily disabled scope functions can be inaccessible outside of the action.
+ */
+export function disableScopeFunctions(action: () => Promise<any>): Promise<void>;
+export function disableScopeFunctions(action?: () => void | Promise<any>): void | Promise<void> {
+	disable();
+	if (!action)
+		return;
+	const result = action();
+	if (result instanceof Promise)
+		return result.then(enable);
+	enable();
+}
+
+// Enable scope functions by default
+enable();
